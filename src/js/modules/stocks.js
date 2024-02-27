bc355f75f34b60a95bf0a211aad3427a76c61f05
Astemir Gurizhev <gurizhev.astema@mail.ru>
2024-02-27 16:56:32 +0300

    change project

---
+++ src/js/modules/stocks.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper(".swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next", // класс нашего элемента, который яв. кнопкой
+    prevEl: ".swiper-button-prev", // класс нашего элемента, который яв. кнопкой
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 36,
+    },
+    0: {
+        slidesPerView: 1,
+        spaceBetween: 18
+    }
+  }
+});
