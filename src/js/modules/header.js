e99d85947033c2f7877a0cd530838a508a29d235
Astemir Gurizhev <gurizhev.astema@mail.ru>
2024-02-29 14:04:25 +0300

    update

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,3 +1,5 @@
+const { doWhilst, log } = require("neo-async");
+
 let lastScrollTop = 0;
 window.addEventListener("scroll", () => {
   let st = window.pageYOffset || document.documentElement.scrollTop;
@@ -8,3 +10,22 @@ window.addEventListener("scroll", () => {
   }
   lastScrollTop = st <= 0 ? 0 : st; 
 }, false);
+
+
+const headerBurger = document.querySelector('.header__burger')
+const headerBurgerMenu = document.querySelector('.header__burger-menu')
+const headerMenu = document.querySelector('.header__menu')
+const headerLogo = document.querySelector('.header__logo')
+const headerRight = document.querySelector('.header__right')
+const headerRow = document.querySelector('.header__row')
+const body = document.querySelector('body')
+console.log(headerBurger);
+headerBurger.addEventListener('click', ()=>{
+  headerBurger.classList.toggle('active')
+  headerBurgerMenu.classList.toggle('active')
+  headerRow.classList.toggle('active')
+  body.classList.toggle('menu-open')
+  headerLogo.classList.toggle('none')
+  headerMenu.classList.toggle('none')
+  headerRight.classList.toggle('none')
+})
